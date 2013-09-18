ff9a54f9f8754e9d3839b96d3d4d6da89fe147f8
Code Ninja <ninja@naruto.ccsys.com>
2013-09-18 08:32:57 +0000

    add port/package hacks for postgresql and roundcube

---
+++ pkg/databases_postgresql92-server.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/false
+set -e; set -u; set -C
+
+# nuOS 0.0.9.2b1 - pkg/databases_postgresql92-server.sh - LICENSE: MOZ_PUB
+#
+# Copyright (c) 2008-2013 Chad Jacob Milios and Crop Circle Systems, Inc.
+# All rights reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v2.0.
+# If a copy of the MPL was not distributed alongside this file, you can obtain one at
+# http://mozilla.org/MPL/2.0/ . This software project is not affiliated with the Mozilla
+# Foundation.
+#
+# Official updates and community support available at http://nuos.org .
+# Other licensing options and professional services available at http://ccsys.com .
+
+[ $NUOS_VER = 0.0.9.2b1 ]
+
+case $pkg_step in
+	post-build|post-install)
+		mv "${destdir-}/usr/local/pgsql" "${destdir-}/var/db/"
+		${destdir:+chroot "$destdir"} pw usermod pgsql -d /var/db/pgsql
+		;;
+esac
---
+++ pkg/mail_roundcube.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/false
+set -e; set -u; set -C
+
+# nuOS 0.0.9.2b1 - pkg/mail_roundcube.sh - LICENSE: MOZ_PUB
+#
+# Copyright (c) 2008-2013 Chad Jacob Milios and Crop Circle Systems, Inc.
+# All rights reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v2.0.
+# If a copy of the MPL was not distributed alongside this file, you can obtain one at
+# http://mozilla.org/MPL/2.0/ . This software project is not affiliated with the Mozilla
+# Foundation.
+#
+# Official updates and community support available at http://nuos.org .
+# Other licensing options and professional services available at http://ccsys.com .
+
+[ $NUOS_VER = 0.0.9.2b1 ]
+
+case $pkg_step in
+	post-install)
+		${destdir:+chroot "$destdir"} chown www:www /usr/local/www/roundcube/temp /usr/local/www/roundcube/logs
+		;&
+	post-build)
+		mkdir -p "${destdir-}/usr/local/etc/roundcube"
+		ln -s ../../../etc/roundcube/main.inc.php "${destdir-}/usr/local/www/roundcube/config/"
+		ln -s ../../../etc/roundcube/db.inc.php "${destdir-}/usr/local/www/roundcube/config/"
+		mkdir -p "${destdir-}/var/roundcube"
+		mv "${destdir-}/usr/local/www/roundcube/temp" "${destdir-}/var/roundcube/" || (rm "${destdir-}/usr/local/www/roundcube/temp/.htaccess" && rmdir "${destdir-}/usr/local/www/roundcube/temp")
+		ln -s ../../../../var/roundcube/temp "${destdir-}/usr/local/www/roundcube/"
+		[ ! -e "${destdir-}/var/log/roundcube" ] && mv "${destdir-}/usr/local/www/roundcube/logs" "${destdir-}/var/log/roundcube" || (rm "${destdir-}/usr/local/www/roundcube/logs/.htaccess" && rmdir "${destdir-}/usr/local/www/roundcube/logs")
+		ln -s ../../../../var/log/roundcube "${destdir-}/usr/local/www/roundcube/logs"
+		mkdir -p "${destdir-}/var/db/roundcube"
+		chown www:www "${destdir-}/var/db/roundcube"
+		;;
+esac
