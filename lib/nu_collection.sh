1a098415c359d1fe5eefc547c1a2f96448cdea0a
Chad Jacob Milios <chad@danzo.ccsys.com>
2015-07-12 22:15:15 +0000

    add sudo to standard tools

--- lib/nu_collection.sh
+++ lib/nu_collection.sh
@@ -27,7 +27,7 @@ nuos_lib_collection_loaded=y
 collection_vars_init () {
 	COLL_blank=
 	COLL_bare='blank sysutils/memtest86+ sysutils/jail2'
-	COLL_lite='bare sysutils/screen misc/buffer sysutils/pipemeter archivers/lzop sysutils/smartmontools net/rsync'
+	COLL_lite='bare sysutils/screen misc/buffer sysutils/pipemeter archivers/lzop sysutils/smartmontools net/rsync security/sudo'
 	COLL_developer='lite devel/subversion devel/git'
 	COLL_server='developer mail/postfix sysutils/pefs-kmod security/gnupg dns/unbound dns/djbdns security/openvpn ftp/pure-ftpd net/isc-dhcp42-server net/netatalk3 net/samba36 security/tor net-p2p/rtorrent net-p2p/bitcoin-daemon net-p2p/bitcoin-utils finance/vanitygen net-p2p/namecoin-daemon net-p2p/litecoin-daemon multimedia/ffmpeg lang/expect databases/postgresql94-server databases/mysql56-server databases/mongodb databases/redis www/npm www/apache24 www/nginx mail/cyrus-imapd25 security/cyrus-sasl2-saslauthd net-im/ejabberd lang/phantomjs lang/clojure textproc/rubygem-sass emulators/virtualbox-ose graphics/povray-meta graphics/graphviz x11-fonts/webfonts lang/php56-extensions www/mod_php56 mail/roundcube-sieverules irc/irssi'
 	COLL_desktop='server graphics/gimp x11/xorg x11/kde4 databases/virtuoso net-p2p/bitcoin net-p2p/namecoin net-p2p/litecoin net-p2p/retroshare net/x11vnc net/tightvnc www/firefox mail/thunderbird editors/libreoffice multimedia/vlc net-p2p/qbittorrent irc/kvirc'
