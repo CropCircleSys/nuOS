22093fb54fbb9722f6bd2cb6453abc5e85b3f3cd
Boba Fett <boba@jack.ccsys.com>
2018-02-25 22:54:26 +0000

    mo betta

--- www/src/App.vue
+++ www/src/App.vue
@@ -1,13 +1,22 @@
 <template>
 	<div id="app">
-		<header><span class="title"><strong><em>nu</em>OS</strong>.org</span></header>
-		<nav></nav>
+		<header>
+			<span class="title"><strong><em>nu</em>OS</strong>.org</span>
+			<nav>
+				<ul>
+					<li>home</li>
+					<li>information</li>
+					<li>download</li>
+					<li>source-code</li>
+				</ul>
+			</nav>
+		</header>
 		<router-view/>
 		<footer>
 			<p class="brought">
 				<strong>&copy;</strong> MMXVIII<br>
 				<em>brought to you by:</em><br>
-				Crop Circle Systems, Inc. &amp;</li><br>
+				<a href="https://ccsys.com/">Crop Circle Systems, Inc.</a> &amp;<br>
 				the <strong>society</strong> of <strong>independent</strong><br>
 				or <strong>anonymous ethical hackers</strong>
 			</p>
@@ -27,14 +36,20 @@ html {
 	font-size: 12pt;
 	line-height: 1.125;
 }
+a {
+	color: Gray;
+}
+a:hover {
+	color: DarkGray;
+}
 body {
 	background: Black;
 	font-size: 0;
+	color: Gray;
 }
 #app {
 	text-align: center;
 	font-family: "Lucida Bright", Georgia, serif;
-	color: Gray;
 }
 header, article
 {
@@ -44,6 +59,7 @@ header {
 	position: fixed;
 	top: 0;
 	left: 0;
+	text-align: left;
 	margin: 1rem;
 }
 header .title strong {
@@ -58,21 +74,35 @@ nav, aside, footer {
 .title {
 	font-family: Baskerville, "Times New Roman", serif;
 }
+nav {
+	display: inline-block;
+}
+nav ul {
+	margin: 0;
+	padding: 0 0 0 1em;
+}
+nav ul li {
+	display: inline-block;
+	list-style: none;
+	margin: 0 1em;
+	font-weight: bold;
+	color: LightGray;
+}
 footer {
-	position: relative;
 	bottom: 0;
 	width: 100%;
+	margin-top: 4rem;
 }
 .brought {
+	color: DimGray;
 	text-align: right;
 	right: 0;
-	bottom: 0;
 	margin: 1rem;
 }
-.brought ul {
-	margin: 0;
-}
-.brought ul li {
-	list-style: none;
+@media screen and (min-width: 769px) {
+	footer {
+		position: fixed;
+		z-index: -1;
+	}
 }
 </style>
