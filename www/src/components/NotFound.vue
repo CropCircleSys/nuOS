67d9a74ade6860a88306333562dff534ea1916ff
Boba Fett <boba@jack.ccsys.com>
2018-02-27 13:41:04 +0000

    enable HTML5 history mode

---
+++ www/src/components/NotFound.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="home">
+		<div class="title">
+			<h1 class="title">¯\_(ツ)_/¯</h1>
+			<h2><abbr title="What the Fuck?">WTF?</abbr></h2>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'NotFound',
+	data () {
+		return {
+		}
+	}
+}
+</script>
+
+<style scoped>
+div.title {
+	font-size: 3.5vmin;
+	color: LightGray;
+}
+h1.title {
+	font-size: 500%;
+	margin: 1.5em 0 0.33em;
+}
+div.title h2 {
+	font-size: 200%;
+}
+abbr {
+	text-decoration: none;
+}
+</style>
--- www/src/router/index.js
+++ www/src/router/index.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import HelloWorld from '@/components/HelloWorld'
+import NotFound from '@/components/NotFound'
 
 Vue.use(Router)
 
 export default new Router({
+	mode: 'history',
+	// Apache config:
+	// RewriteCond "%{LA-U:REQUEST_FILENAME}" !-f
+	// RewriteCond "%{LA-U:REQUEST_FILENAME}" !-d
+	// RewriteRule . "%{CONTEXT_DOCUMENT_ROOT}/index.html" [nosubreq]
 	routes: [
 		{
 			path: '/',
@@ -16,6 +22,10 @@ export default new Router({
 			path: '/hello',
 			name: 'HelloWorld',
 			component: HelloWorld
+		},
+		{
+			path: '*',
+			component: NotFound
 		}
 	]
 })
