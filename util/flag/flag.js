69f37857bcde5a9efaffe42c71a5a1a98cda8e00
Boba Fett <boba@jack.ccsys.com>
2018-07-05 03:33:25 +0000

    add -v flag for vertical flag

--- util/flag/flag.js
+++ util/flag/flag.js
@@ -3,6 +3,29 @@ const jsdom = require('jsdom').JSDOM;
 
 const argv = process.argv.slice(2);
 
+var tr, vert = false;
+
+if (argv[0] == '-h' || argv[0] == '--') {
+	argv.shift();
+} else if (argv[0] == '-v') {
+	vert = true
+	argv.shift();
+}
+
+if (vert) {
+	tr = '';
+	qtr = '';
+	vb = '1000 1900';
+	qw = 538;
+	qh = 760;
+} else {
+	tr = ' transform="rotate(-90) translate(-1000 0)"';
+	qtr = ' transform="rotate(90 950 500) translate(450 450)"';
+	vb = '1900 1000';
+	qw = 760;
+	qh = 538;
+}
+
 const barCode = barcode(
 	argv[0],
 	'code128',
@@ -16,8 +39,8 @@ const barCode = barcode(
 const qrCode = qrcode({
 	content: argv[1],
 	padding: 3,
-	width: 760,
-	height: 538,
+	width: qw,
+	height: qh,
 	color: '#ffffff',
 	background: '#0000ff',
 	ecl: 'H'
@@ -26,13 +49,13 @@ const qrCode = qrcode({
 const bd = jsdom.fragment(barCode);
 const qd = jsdom.fragment(qrCode);
 
-var flag = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1900 1000"><g transform="rotate(-90) translate(-1000 0)"><g>';
+var flag = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 ' + vb + '"><g' + tr + '><g>';
 
 for (var n of bd.childNodes[0].childNodes) {
 	flag += n.outerHTML;
 }
 
-flag += '</g><g transform="rotate(90 950 500) translate(450 450)">';
+flag += '</g><g' + qtr + '>';
 
 var re = /style="fill: ?#(......); ?shape-rendering: ?crispEdges;?"/g;
 for (var n of qd.childNodes[1].childNodes) {
