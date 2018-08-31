7dd5e3b858ff7e0a4843413e2d50f5e30d245ae8
Jedi Hacker <jedi@yoda.ccsys.com>
2018-08-31 03:57:25 +0000

    lil mo betta

--- share/examples/install/soho_fileserver.sh
+++ share/examples/install/soho_fileserver.sh
@@ -1,4 +1,12 @@
 nu_ns_cache -C "$TRGT" -s
 enable_svc -C "$TRGT" openssh ntpd dhcpcd dbus avahi_daemon netatalk
+
 sed -Ee 's/^\+(afp interfaces)/+;\1/' "$NUOS"/share/examples/etc/afp.conf.diff | patch "$TRGT"/usr/local/etc/afp.conf
+
+cat >> "$TRGT"/usr/local/etc/dhcpcd.conf <<'EOF'
+
+# Ignore VPN interfaces
+denyinterfaces tap[0-9]* tun[0-9]*
+EOF
+
 cp "$NUOS"/share/examples/etc/dhcpcd.exit-hook "$TRGT"/usr/local/etc/
