f297b6399ed48b8fa73b6c8b66c9e5e1d04f7686
Jedi Hacker <jedi@yoda.ccsys.com>
2018-09-28 13:41:05 +0000

    lil mo betta

--- share/examples/install/soho_fileserver.sh
+++ share/examples/install/soho_fileserver.sh
@@ -6,7 +6,7 @@ sed -Ee 's/^\+(afp interfaces)/+;\1/' "$NUOS"/share/examples/etc/afp.conf.diff |
 cat >> "$TRGT"/usr/local/etc/dhcpcd.conf <<'EOF'
 
 # Ignore VPN interfaces
-denyinterfaces tap[0-9]* tun[0-9]*
+denyinterfaces tap[0-9]* tun[0-9]* ng[0-9]*
 EOF
 
 cp "$NUOS"/share/examples/etc/dhcpcd.exit-hook "$TRGT"/usr/local/etc/
