2567a3f66001091bc939f8aa18a4f9817f3310c3
Jedi Hacker <jedi@yoda.ccsys.com>
2019-07-24 02:25:05 +0000

    first hack at FreeBSD 11.3 support

--- lib/nu_ports.sh
+++ lib/nu_ports.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/false
 set -e; set -u; set -C
 
-# nuOS 0.0.11.2a1 - lib/nu_ports.sh
+# nuOS 0.0.11.3a0 - lib/nu_ports.sh
 #
-# Copyright (c) 2008-2018 Chad Jacob Milios and Crop Circle Systems.
+# Copyright (c) 2008-2019 Chad Jacob Milios and Crop Circle Systems.
 # All rights reserved.
 #
 # This Source Code Form is subject to the terms of the Simplified BSD License.
@@ -14,7 +14,7 @@ set -e; set -u; set -C
 # Official updates and community support available at https://nuos.org .
 # Professional services available at https://ccsys.com .
 
-nuos_lib_ver=0.0.11.2a1
+nuos_lib_ver=0.0.11.3a0
 [ $nuos_lib_ver = "$NUOS_VER" ]
 [ -n "${nuos_lib_system_loaded-}" ]
 [ -n "${nuos_lib_make_loaded-}" ]
