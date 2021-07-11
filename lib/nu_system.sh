f8ca6d66cb0215410ee26c61e06cedbde4dd9263
Jedi Hacker <jedi@artu.macleod.host>
2021-07-11 02:51:24 +0000

    combine lib/system and lib/common

--- lib/nu_system.sh
+++ lib/nu_system.sh
@@ -17,7 +17,9 @@ set -e; set -u; set -C
 nuos_lib_ver=0.0.11.4a0
 [ $nuos_lib_ver = "$NUOS_VER" ]
 [ -z "${nuos_lib_system_loaded-}" ]
+[ -z "${nuos_lib_common_loaded-}" ]
 nuos_lib_system_loaded=y
+nuos_lib_common_loaded=y
 
 : ${TMPDIR:=/tmp}
 
@@ -100,6 +102,21 @@ push () {
 	prepend="${old_val:+$old_val }"
 	new_val="$prepend$*"
 	setvar $var "$new_val"
+}
+
+eko () {
+	printf '%s\n' "$*"
+}
+
+re_pattern () {
+	local whole=;
+	if [ "$1" = -n ]; then
+		shift
+	else
+		whole=y
+	fi
+	local var=$1; shift
+	setvar ${var}_re "${whole:+^}`eval eko \\"\\$$var\\" | sed -e 's/\./\\\./g'`${whole:+\$}"
 }
 
 humanize () {
@@ -332,4 +349,120 @@ save_svn_info () {
 		: ${r:=0}
 		echo r$r
 	fi
+}
+
+
+nuos_init () {
+	for conf_file in /usr/nuos/conf /etc/nuos.conf /etc/nuos/conf; do
+		if [ -r "$conf_file" ]; then
+			. "$conf_file"
+		fi
+		if [ -r "${CHROOTDIR-}$conf_file" ]; then
+			. "${CHROOTDIR-}$conf_file"
+		fi
+	done
+	
+	: ${NUOS_SUPPORTED:=UNSUPPORTED}
+	: ${HOSTOS_TYPE:=$BASEOS_TYPE}
+	: ${HOSTOS_VER:=$BASEOS_VER}
+	: ${HOSTOS_ARCH:=`uname -m`}
+	: ${HOSTOS_PROC:=`uname -p`}
+	if [ $HOSTOS_ARCH = $HOSTOS_PROC ]; then
+		HOSTOS_MACH=$HOSTOS_ARCH
+	else
+		HOSTOS_MACH=$HOSTOS_ARCH.$HOSTOS_PROC
+	fi
+	if [ -q != "${1-}" ]; then
+		echo 'nuos app v#                       ' $NUOS_VER
+		echo 'nuos support       NUOS_SUPPORTED ' "$NUOS_SUPPORTED"
+		echo 'host opsys                        ' "$HOSTOS_TYPE"
+		echo 'host opsys v#                     ' $HOSTOS_VER
+		echo "host pkg collec'n                 " ${HOSTOS_PKG_COLLECTION-n/a}
+	fi
+}
+
+nuos_ssl_init () {
+	if [ -x /usr/local/bin/openssl ]; then
+		SSL_CMD=/usr/local/bin/openssl
+		SSL_SUITE=openssl-port
+		SSL_CONF=/usr/local/openssl/openssl.cnf
+		[ -e $SSL_CONF ] || cp $SSL_CONF.sample $SSL_CONF
+	else
+		SSL_CMD=/usr/bin/openssl
+		SSL_SUITE=openssl-freebsd-base
+		SSL_CONF=/etc/ssl/openssl.cnf
+	fi
+	: ${HOST:=`hostname`}
+	if [ ${HOST%%.*} != "`readlink /etc/ssl/certs.installed/localhost`" ]; then
+		mkdir -p /etc/ssl/certs.installed/${HOST%%.*}
+		ln -sf ${HOST%%.*} /etc/ssl/certs.installed/localhost
+	fi
+}
+
+nuos_ssh_init () {
+	if [ -x /usr/local/bin/ssh ]; then
+		SSH_CMD=/usr/local/bin/ssh
+		SSH_SUITE=openssh-port
+	else
+		SSH_CMD=/usr/bin/ssh
+		SSH_SUITE=openssh-freebsd-base
+	fi
+}
+
+nuos_sha_fngr () {
+	local bytes=24
+	while getopts b:f OPT; do case $OPT in
+		b) bytes=$OPTARG; [ $bytes -ge 1 -a $bytes -le 43 ];;
+		f) opt_force=y;;
+	esac; done; shift $(($OPTIND-1))
+	[ $# -ge 1 ]
+	[ $bytes -le 42 -o -n "${opt_force-}" ]
+	
+	cat "$@" 2>/dev/null |
+		sha256 -q |
+		(echo 16i; echo -n FF; tr a-f A-F; echo P) | dc | tail -c +2 |
+		b64encode - |
+		sed -nEe "
+			/^begin-base64 /{
+				n
+				s/=?=\$//
+				y|+/|-_|
+				s/^(.{$bytes}).*\$/\1/
+				p
+				q
+			}"
+}
+
+ns_master_zone () {
+	local opt_chroot= alternate= chrootdir= zone= host_name=; unset alternate chrootdir
+	while getopts A:cC:j: OPT; do case $OPT in
+		A) alternate=$OPTARG;;
+		c) opt_chroot=y;;
+		C) chrootdir=$OPTARG;;
+		j) jailname=$OPTARG;;
+	esac; done; shift $(($OPTIND-1))
+	[ $# -eq 1 ]
+	host_name=$1; shift
+	zone=$host_name
+	
+	if [ -n "${jailname-}" ]; then
+		chrootdir="`jls -j $jailname path`"
+		opt_chroot=y
+		[ -n "$chrootdir" ] || { echo "could not find running jail thusly named." >&2 && return 85; }
+	fi
+	
+	while [ ! -f "${opt_chroot:+${chrootdir-$CHROOTDIR}}/var/db/knot${alternate:+_${alternate}}/$zone.zone" ]; do
+		echo $zone | grep -q '\.' || { echo "could not find zone file (are we the configured master of a parent zone?)" >&2 && return 85; }
+		zone=${zone#*.}
+	done
+	echo $zone
+}
+
+set_primary_phys_netif () {
+	local primary_if=$1
+	local trgt=${2-}
+	sed -i '' -E -e '/^ifconfig_.+_name="?net0"?/d' "$trgt/etc/rc.conf.local"
+	cat >> "$trgt/etc/rc.conf.local" <<EOF
+ifconfig_${primary_if}_name="net0"
+EOF
 }
